b3ef5d031dc5edb64d4c5982c4f76577fa2dc4d8
naufal-dean <naufal.dean@gmail.com>
2020-04-07 15:10:18 +0700

    Add assets

--- src/main/java/com/avatarduel/controller/CardController.java
+++ src/main/java/com/avatarduel/controller/CardController.java
@@ -4,6 +4,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.avatarduel.AvatarDuel;
+import com.avatarduel.model.CardType;
+import com.avatarduel.model.Element;
 import com.sun.javafx.scene.control.skin.Utils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -11,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 import com.avatarduel.model.Card;
@@ -65,13 +68,38 @@ public class CardController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle resources) {
-        this.background.setImage(new Image(AvatarDuel.class.getResource("card/template/character_template.png").toString()));
-        this.name.setFont(Font.loadFont(AvatarDuel.class.getResourceAsStream("font/palatino-linotype-bold.ttf"), 13));
+        // Set background
+        String templatePath;
+        if (this.card.getCardType() == CardType.CHARACTER)
+            templatePath = "card/template/template_character.png";
+        else if (this.card.getCardType() == CardType.LAND)
+            templatePath = "card/template/template_land.png";
+        else // this.card.getCardType() == CardType.SKILL
+            templatePath = "card/template/template_skill.png";
+        this.background.setImage(new Image(AvatarDuel.class.getResource(templatePath).toString()));
+        // Set element
+        String elementPath;
+        if (this.card.getElement() == Element.AIR)
+            elementPath = "card/template/element_air.png";
+        else if (this.card.getElement() == Element.EARTH)
+            elementPath = "card/template/element_earth.png";
+        else if (this.card.getElement() == Element.FIRE)
+            elementPath = "card/template/element_fire.png";
+        else // this.card.getElement() == Element.WATER
+            elementPath = "card/template/element_water.png";
+        this.element.setImage(new Image(AvatarDuel.class.getResource(elementPath).toString()));
+        // Set name
+        this.name.setFont(Font.loadFont(AvatarDuel.class.getResourceAsStream("font/palatino-linotype-bold.ttf"), 12));
         this.name.setText(this.card.getName().toUpperCase());
+        if (this.card.getCardType() != CardType.CHARACTER)
+            this.name.setTextFill(Color.WHITE);
         this.name.setPrefWidth(this.background.getFitWidth() * 0.7);
+        // Set image
         this.image.setImage(new Image(this.card.getImagePath()));
+        // Set description
         this.description.setDisable(true);
         this.description.setWrapText(true);
+        this.description.setFont(Font.loadFont(AvatarDuel.class.getResourceAsStream("font/palatino-linotype.ttf"), 9));
         this.description.setText(this.card.getDescription());
     }
 }
