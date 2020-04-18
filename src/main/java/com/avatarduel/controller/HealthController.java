9480132bb2ced4597b9b6164c509b040fa3da82b
naufal-dean <naufal.dean@gmail.com>
2020-04-18 20:28:43 +0700

    Resize all assets to improve performance

--- src/main/java/com/avatarduel/controller/HealthController.java
+++ src/main/java/com/avatarduel/controller/HealthController.java
@@ -3,17 +3,21 @@ package com.avatarduel.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.avatarduel.model.Player;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 import com.avatarduel.AvatarDuel;
+import com.avatarduel.model.Player;
 import com.avatarduel.gameutils.GameStatus;
-import javafx.scene.text.Font;
+import javafx.util.Duration;
 
 
 public class HealthController implements Initializable {
@@ -22,13 +26,21 @@ public class HealthController implements Initializable {
      */
     private Player owner;
     /**
+     * Health change animation
+     */
+    private Timeline healthChange;
+    /**
      * Health Bar root pane
      */
     @FXML private StackPane healthRoot;
     /**
+     * Health bar pane
+     */
+    @FXML private Pane healthBarPane;
+    /**
      * Health Bar image
      */
-    @FXML private ImageView healthVisual;
+    @FXML private Rectangle healthBar;
     /**
      * Health text label
      */
@@ -49,9 +61,22 @@ public class HealthController implements Initializable {
         int healthPoint = GameStatus.getGameStatus().getGameHealthMap().get(this.owner);
         if (healthPoint < 0) healthPoint = 0;
         if (healthPoint > 80) healthPoint = 80;
-        String templatePath = "img/health/health_bar_" + healthPoint + ".png";
-        this.healthVisual.setImage(new Image(AvatarDuel.class.getResource(templatePath).toString()));
         this.healthRemain.setText(String.valueOf(healthPoint) + "/80");
+        // Health bar
+        // Animation
+        double oldWidth = this.healthBar.getWidth();
+        double newWidth = this.healthBarPane.getPrefWidth() * healthPoint / 80d;
+        this.healthChange = new Timeline(
+                new KeyFrame(
+                        Duration.ZERO,
+                        new KeyValue(this.healthBar.widthProperty(), oldWidth)
+                ),
+                new KeyFrame(
+                        Duration.millis(500),
+                        new KeyValue(this.healthBar.widthProperty(), newWidth)
+                )
+        );
+        this.healthChange.play();
     }
 
     /**
@@ -61,6 +86,7 @@ public class HealthController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.healthLabel.setFont(Font.loadFont(AvatarDuel.class.getResourceAsStream("font/palatino-linotype-bold.ttf"), 48));
         this.healthRemain.setFont(Font.loadFont(AvatarDuel.class.getResourceAsStream("font/palatino-linotype-bold.ttf"), 48));
+        this.healthBar.setWidth(0);
         this.init();
     }
 }
